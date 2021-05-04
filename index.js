3d25069b39ba314f311666842d1174da769c0425
sin min gyu <>
2021-05-04 05:45:47 +0000

    finish index

--- index.js
+++ index.js
@@ -98,16 +98,102 @@ const plus = calculator.plus(5, 5)
 console.log(plus);
 */
 
-const title = document.querySelector("#title");
-
 /*
+const title = document.getElementById("title");
+
 title.innerHTML = "Hi From JS";
 title.style.color = "red";
 document.title = "I own you now";
 */
 
-function handleResize() {
+/*
+const title = document.querySelector("#title");
+
+function handleClick() {
+    title.style.color = "yellow";
     console.log("I have been resized")
 }
 
-window.addEventListener("resize", handleResize);
+title.addEventListener("click", handleClick);
+*/
+
+ /*
+if ('10' === 10) {
+    console.log('hi');
+} else if ("10" === "11") {
+    console.log('lalalala');
+} else {
+    console.log("ho");
+}
+*/
+
+/*
+if (20 > 5 || "mingyu" === "minkyu") {
+    console.log('yes');
+} else {
+    console.log("no");
+}
+*/
+
+/*
+const age = prompt("How old are you");
+
+if (age > 18 && age < 21) {
+    console.log("you can drink but you should not");
+} else if (age > 20) {
+    console.log("you ahead");
+} else if (age < 18) {
+    console.log("you are too young");
+}
+*/
+
+/*
+const title = document.querySelector("#title");
+
+const BASE_COLOR = "yellow";
+const OTHER_COLOR = "#7f8c8d";
+
+function handleClick() {
+    const currentColor = title.style.color;
+    if (currentColor === BASE_COLOR){
+        title.style.color = OTHER_COLOR;
+    } else {
+        title.style.color = BASE_COLOR;
+    }
+}
+
+function init(){
+    title.style.color = BASE_COLOR;
+    title.addEventListener("click", handleClick);
+}
+init();
+
+function handleOffline() {
+    console.log("good bye!");
+}
+
+function handleOnline() {
+    console.log("Welcome back!");
+}
+
+window.addEventListener("offline", handleOffline);
+window.addEventListener("online", handleOnline);
+*/
+
+const title = document.querySelector("#title");
+
+const CLICKED_CLASS = "clicked";
+
+function handleClick() {
+    const currentClass = title.className;
+    if(currentClass !== CLICKED_CLASS){
+        title.className = CLICKED_CLASS;
+    } else {
+        title.className = "";
+    }
+}
+
+function init(){
+    title.addEventListener("click", handleClick);
+}
+init();
